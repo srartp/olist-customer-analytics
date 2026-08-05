7dc6affd1096163b0b1c1a1719664e396f495e2c
Sara El Kadoussi <saraelkadoussi14@gmail.com>
2026-08-05 16:23:14 +0100

    feat: add retention rate percentage to cohort mart

--- dbt_project/models/marts/mart_cohort_retention.sql
+++ dbt_project/models/marts/mart_cohort_retention.sql
@@ -40,7 +40,32 @@ cohort_activity as (
     from orders_with_cohort
     group by cohort_month, order_month
 
+),
+
+cohort_size as (
+
+    select
+        cohort_month,
+        n_active_customers as cohort_initial_size
+    from cohort_activity
+    where month_number = 0
+
+),
+
+retention_rates as (
+
+    select
+        ca.cohort_month,
+        ca.order_month,
+        ca.month_number,
+        ca.n_active_customers,
+        cs.cohort_initial_size,
+        round(ca.n_active_customers * 100.0 / cs.cohort_initial_size, 2) as retention_rate_pct
+    from cohort_activity ca
+    left join cohort_size cs on ca.cohort_month = cs.cohort_month
+
 )
 
-select * from cohort_activity
+select * from retention_rates
 order by cohort_month, month_number
+
